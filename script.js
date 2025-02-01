2712cd871a3ad0e48824db2005f82a96ba4039f3
Sonu Singha <deyk64032@gmail.com>
2025-02-01 23:58:03 +0530

    JS logic for PDF input added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // CODE STARTS FROM HERE
+
+console.log(PDFLib)
+
+
+
+
+
+
+
+// ----------------------------------------------------------------------Selecting Elements
+
+let Choose_File = document.body.querySelector(".Choose-File");
+
